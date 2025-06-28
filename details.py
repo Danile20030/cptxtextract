295930f9d81a0b3eea350f70f21fd87dd14e1a77
Danile20030 <114801076+Danile20030@users.noreply.github.com>
2025-06-28 18:01:59 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,10 +2,10 @@ import os
 from os import getenv
 
 
-API_ID = int(getenv("API_ID", 25218674))
-API_HASH = getenv("API_HASH", "87c231f0e8704795b8239d06965b4351")
-BOT_TOKEN = getenv("BOT_TOKEN", "7198600769:AAGW4WAlCWppTjwTceBxWqDwpWP7UCEWuzI")
-OWNER_ID = int(getenv("OWNER_ID", "6805344720"))
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "999741495  20000898623").split()))
+API_ID = int(getenv("API_ID", 21702398))
+API_HASH = getenv("API_HASH", "70b0cc4ab3e1f29aa2d386883adeda38")
+BOT_TOKEN = getenv("BOT_TOKEN", "8118953747:AAH_F8eClQ6C_x1j0SHU1g_7b-Xfw6M_2JU")
+OWNER_ID = int(getenv("OWNER_ID", "7320087240"))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7320087240").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002038285084"))
+CHANNEL_ID = int(getenv("CHANNEL_ID", "8118953747"))
